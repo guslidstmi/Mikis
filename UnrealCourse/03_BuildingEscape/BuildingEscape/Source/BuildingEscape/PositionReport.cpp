84cc97856e58238793130a5da6adeba2a2de1532
Mikaela Lidström <m.s.lidstrom@gmail.com>
2016-04-09 16:01:33 +0200

    Added Behaviour to Door
    Door can open

--- UnrealCourse/03_BuildingEscape/BuildingEscape/Source/BuildingEscape/PositionReport.cpp
+++ UnrealCourse/03_BuildingEscape/BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -21,7 +21,9 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	FString ObjectName = GetOwner()->GetName();
+	FString ObjectPos = GetOwner()->GetActorLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *ObjectName, *ObjectPos);
 	
 }
 
