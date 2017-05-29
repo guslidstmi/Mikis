564016f0ad81d03f1b4dfeb59f025d20208be031
Mikaela Lidström <m.s.lidstrom@gmail.com>
2017-05-29 16:08:18 +0100

    Add visual feedback when enemies get damaged

--- Games/Spaceinv/Enemy.cpp
+++ Games/Spaceinv/Enemy.cpp
@@ -2,18 +2,22 @@
 
 Enemy::Enemy(const float x, const float y, const int id, const int row, const int col) 
 	: m_enemy{}
-	, left{ 0, 0, 25, 25 }
-	, front{28, 0, 25, 25} 
-	, right{52, 0, 25, 25}
-	, explo1{75, 0, 25, 25} 
-	, explo2{100, 0, 25, 25}
-	, explo3{125, 0, 25, 25}
+	, left{ 6, 8, 45, 40 }
+	, front{66, 8, 45, 40 }
+	, right{130, 8, 45, 40 }
+	, left2{6, 65, 45, 40 }
+	, front2{66, 65, 45, 40 }
+	, right2{130, 65, 45, 40 }
+	, left3{6, 130, 45, 40 }
+	, front3{66, 130, 45, 40 }
+	, right3{130, 130, 45, 40 }
 	, m_id{ id }
 	, m_row{ row }
 	, m_col{ col }
 	, m_lives{ 3 }
 	, m_destroyed{ false }
 	, m_bottom{ false }
+	, m_state{State::FullHealth}
 {
 	m_enemy.setPosition(x, y);
 }
@@ -23,7 +27,7 @@ const bool Enemy::update(const int num, const sf::Texture& texture)
 	if (m_enemy.getTexture() == nullptr)
 	{
 		m_enemy.setTexture(texture);
-		m_enemy.scale(1.7f, 1.7f);
+		m_enemy.scale(1.0f, 1.0f);
 		m_enemy.setTextureRect(front);
 	}
 	if (m_enemy.getPosition().y > 525.f && !isDestroyed())
@@ -41,31 +45,57 @@ const bool Enemy::update(const int num, const sf::Texture& texture)
 			velocity.y += 20.f;
 			velocity.x = 0;
 			m_enemy.move(velocity);
-			m_enemy.setTextureRect(front);
+			switch (m_state)
+			{
+			case State::FullHealth : m_enemy.setTextureRect(front); break;
+			case State::HitOnce :  m_enemy.setTextureRect(front2); break;
+			case State::HitTwice :  m_enemy.setTextureRect(front3); break;
+			} 
+			
 			break;
 		case 1 :
 			velocity.y = 0;
 			velocity.x += 15.f;
 			m_enemy.move(velocity);
-			m_enemy.setTextureRect(right);
+			switch (m_state)
+			{
+			case State::FullHealth: m_enemy.setTextureRect(right); break;
+			case State::HitOnce:  m_enemy.setTextureRect(right2); break;
+			case State::HitTwice:  m_enemy.setTextureRect(right3); break;
+			}
 			break;
 		case 2 :
 			velocity.y = 0;
 			velocity.x -= 15.f;
 			m_enemy.move(velocity);
-			m_enemy.setTextureRect(left);
+			switch (m_state)
+			{
+			case State::FullHealth: m_enemy.setTextureRect(left); break;
+			case State::HitOnce:  m_enemy.setTextureRect(left2); break;
+			case State::HitTwice:  m_enemy.setTextureRect(left3); break;
+			}
 			break;
 		case 3 :
 			velocity.y = 0;
 			velocity.x -= 15.f;
 			m_enemy.move(velocity);
-			m_enemy.setTextureRect(left);
+			switch (m_state)
+			{
+			case State::FullHealth: m_enemy.setTextureRect(left); break;
+			case State::HitOnce:  m_enemy.setTextureRect(left2); break;
+			case State::HitTwice:  m_enemy.setTextureRect(left3); break;
+			}
 			break;
 		case 4 :
 			velocity.y = 0;
 			velocity.x += 15.f;
 			m_enemy.move(velocity);
-			m_enemy.setTextureRect(right);
+			switch (m_state)
+			{
+			case State::FullHealth: m_enemy.setTextureRect(right); break;
+			case State::HitOnce:  m_enemy.setTextureRect(right2); break;
+			case State::HitTwice:  m_enemy.setTextureRect(right3); break;
+			}
 			break;
 		}
 		break;
@@ -76,35 +106,71 @@ const bool Enemy::update(const int num, const sf::Texture& texture)
 		case 0:
 			velocity.y += 20.f;
 			velocity.x = 0;
-			m_enemy.move(velocity);
-			m_enemy.setTextureRect(front);
+			m_enemy.move(velocity); 
+			switch (m_state)
+			{
+			case State::FullHealth: m_enemy.setTextureRect(front); break;
+			case State::HitOnce:  m_enemy.setTextureRect(front2); break;
+			case State::HitTwice:  m_enemy.setTextureRect(front3); break;
+			}
 			break;
 		case 1:
 			velocity.y = 0;
 			velocity.x -= 15.f;
 			m_enemy.move(velocity);
-			m_enemy.setTextureRect(left);
+			switch (m_state)
+			{
+			case State::FullHealth: m_enemy.setTextureRect(left); break;
+			case State::HitOnce:  m_enemy.setTextureRect(left2); break;
+			case State::HitTwice:  m_enemy.setTextureRect(left3); break;
+			}
 			break;
 		case 2:
 			velocity.y = 0;
 			velocity.x += 15.f;
 			m_enemy.move(velocity);
-			m_enemy.setTextureRect(right);
+			switch (m_state)
+			{
+			case State::FullHealth: m_enemy.setTextureRect(right); break;
+			case State::HitOnce:  m_enemy.setTextureRect(right2); break;
+			case State::HitTwice:  m_enemy.setTextureRect(right3); break;
+			}
 			break;
 		case 3:
 			velocity.y = 0;
 			velocity.x += 15.f;
 			m_enemy.move(velocity);
-			m_enemy.setTextureRect(right);
+			switch (m_state)
+			{
+			case State::FullHealth: m_enemy.setTextureRect(right); break;
+			case State::HitOnce:  m_enemy.setTextureRect(right2); break;
+			case State::HitTwice:  m_enemy.setTextureRect(right3); break;
+			}
 			break;
 		case 4:
 			velocity.y = 0;
 			velocity.x -= 15.f;
 			m_enemy.move(velocity);
-			m_enemy.setTextureRect(left);
+			switch (m_state)
+			{
+			case State::FullHealth: m_enemy.setTextureRect(left); break;
+			case State::HitOnce:  m_enemy.setTextureRect(left2); break;
+			case State::HitTwice:  m_enemy.setTextureRect(left3); break;
+			}
 			break;
 		}
 		break;
 	}
 	return false;
 }
+
+void Enemy::incrementState()
+{
+	switch (m_state)
+	{
+	case State::FullHealth: m_state = State::HitOnce; break;
+	case State::HitOnce: m_state = State::HitTwice; break;
+	default: m_state = State::FullHealth; break;
+	}
+}
+
--- Games/Spaceinv/Enemy.h
+++ Games/Spaceinv/Enemy.h
@@ -9,6 +9,7 @@ public:
 	Enemy(const float x = 0.0f, const float y = 0.0f, const int id = 0, const int row = 0, const int col = 0);
 	const bool update(const int num, const sf::Texture& texture);
 	sf::Sprite m_enemy;
+	enum class State { FullHealth, HitOnce, HitTwice};
 
 	inline const int getCol() const { return m_col; };
 	inline const bool Enemy::isDestroyed() const { return m_destroyed; }
@@ -17,7 +18,7 @@ public:
 	inline void Enemy::setBottom() { m_bottom = true; }
 	inline const int Enemy::getId() const { return m_id; }
 	inline const int Enemy::isDead() const { return m_lives; }
-	inline void Enemy::setLives() { --m_lives; }
+	inline void Enemy::setLives() { --m_lives; incrementState(); }
 	inline const float Enemy::x() const { return m_enemy.getPosition().x; }
 	inline const float Enemy::y() const { return m_enemy.getPosition().y; }
 
@@ -26,15 +27,21 @@ private:
 	sf::IntRect left;
 	sf::IntRect front;
 	sf::IntRect right;
-	sf::IntRect explo1;
-	sf::IntRect explo2;
-	sf::IntRect explo3;
+	sf::IntRect left2;
+	sf::IntRect front2;
+	sf::IntRect right2;
+	sf::IntRect left3;
+	sf::IntRect front3;
+	sf::IntRect right3;
 	int m_lives;
 	int m_id;
 	int m_row;
 	int m_col;
 	bool m_destroyed;
 	bool m_bottom;
+	State m_state;
+
+	void incrementState();
 };
 
 #endif // ENEMY_H_
